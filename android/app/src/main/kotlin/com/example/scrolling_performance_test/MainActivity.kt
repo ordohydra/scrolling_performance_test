255ab7b836b7c233215cba6096753f605657012e
Виктор Сукочев <vsukochev@yandex.ru>
2025-07-05 11:35:09 +0200

    Initial project

---
+++ android/app/src/main/kotlin/com/example/scrolling_performance_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.scrolling_performance_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:scrolling_performance_test/app.dart';
+
+void main() {
+  runApp(const App());
+}
